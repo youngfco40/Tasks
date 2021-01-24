2a3d3f3a90d7cb46759adfca618b65f1761d38cd
youngfco40 <77794356+youngfco40@users.noreply.github.com>
2021-01-24 15:26:43 -0500

    Jenna Young Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,2 +1,34 @@
 install.packages("swirl")
 library(swirl)
+
+swirl()
+Jenna Young
+1
+2
+1
+1
+1
+5+7
+x <- 5 + 7
+x
+y <- x - 3
+y
+c(1.1, 9, 3.14)
+z <- c(1.1, 9, 3.14)
+?c
+z
+z <- c(555)
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z -1)
+2
+1
+my_sqrt
+my_div <- z/my_sqrt
+3
+my_div
+c(1,2,3,4) + c(0,10
+c(1,2,3,4) + c(0,10,100)
+z * 2 + 1000
+my_div
+2

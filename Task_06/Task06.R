d6f731b026ba1944caf84a258c16ac781537d485
youngfco40 <77794356+youngfco40@users.noreply.github.com>
2021-03-18 21:29:46 -0400

    Jenna Young Task 06 upload

---
+++ Task_06/Task06.R
@@ -0,0 +1,33 @@
+source("http://jonsmitchell.com/code/reformatData07.R")
+source("http://jonsmitchell.com/code/simFxn.R")
+source("http://jonsmitchell.com/code/fxn05.R")
+
+
+plot(1,1, type="n", xlim=c(1998,2013), ylim=c(0,1))
+s <- apply(overallFreq, 2, function(x) lines(overallFreq[,1], x, col=rgb(0,0,0,0.01)))
+
+rescaleFreq <- apply(overallFreq[,3:ncol(overallFreq)], 2, function(x) x-x[1])
+plot(1,1, type="n", xlim=c(1998, 2013), ylim=c(-0.25,0.25))
+s <- apply(rescaleFreq, 2, function(x) lines(overallFreq[,1], x, col=rgb(0,0,0,0.01)))
+dYear <- c()
+dAlleles <- c()
+
+for (i in 3:ncol(overallFreq)) {
+	dYear <- c(dYear, overallFreq[,1])
+	Vec <- overallFreq[,i]
+	Init <- overallFreq[1,i]
+	dAlleles <- c(dAlleles, Vec - Init)
+}
+
+smoothScatter(dYear, dAlleles, colramp = Pal, nbin = 100)
+
+smoothScatter(dYear,  dAlleles, colramp = Pal, nbin= 100, xlab="year", ylab = "change in allele freq. since 1998")
+addFit(nruns =10, n=926, ngens =20, startT = 1997, simCol = "gray40", rescale = TRUE)
+
+plot(alleleFreqs$d_freq, alleleFreqs$d_imm, xlim=c(-0.15, 0.15), xlab="overall freq. change", ylab="freq. change in subset")
+points(alleleFreqs$d_freq, alleleFreqs$d_birth, col= 'blue')
+points(alleleFreqs$d_freq, alleleFreqs$d_surv, col = 'red')
+
+
+
+

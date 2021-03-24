b7e1dbf54de8c45e438d9a41701bcc693fec5728
youngfco40 <77794356+youngfco40@users.noreply.github.com>
2021-03-23 21:01:52 -0400

    Jenna Young Project Analysis

---
+++ Project/Code/ProjectData.R
@@ -0,0 +1,5 @@
+setwd('~/Desktop/Evolution/Project')
+
+ProjectData <- read.csv('Datasetforproject.csv', stringsAsFactors = F)
+
+head(ProjectData, 100)
--- Project/ProjectData.R
+++
@@ -1,5 +0,0 @@
-setwd('~/Desktop/Evolution/Project')
-
-ProjectData <- read.csv('Datasets_for_dryad.csv', stringsAsFactors = F)
-
-head(ProjectData)
